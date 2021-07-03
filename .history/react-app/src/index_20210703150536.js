465ec878b0d80bfe40d929138bc9fe19b788f554
fangliwei <2427980375@qq.com>
2021-07-03 15:09:25 +0800

    初始化react脚手架

---
+++ .history/react-app/src/index_20210703150536.js
@@ -0,0 +1,15 @@
+//引入react核心库
+import React from 'react';
+//引入reactReactDOM
+import ReactDOM from 'react-dom';
+import './index.css';
+//引入App
+import App from './App';
+
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
+
+
