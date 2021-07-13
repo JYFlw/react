6c7d0c21332d6b2151806fe23c00b5d54536c6d0
fangliwei <2427980375@qq.com>
2021-07-13 10:46:52 +0800

    new 删除新增方法

---
+++ .history/react-app/src/components/Item/index_20210713095731.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import './index.css'
+
+export default class Item extends Component {
+    //标识鼠标移入、移出的
+    state = {mouse:false} //标识鼠标移入、移出
+    handleMose = flag => {
+        return () => {
+            this.setState({mouse:flag})
+        }
+    }
+    render() {
+        console.log(this.props)
+        const {mouse} = this.state
+        return (
+            <li style={{backgroundColor:mouse ? '#ddd' : 'white'}} onMouseEnter={this.handleMose(true)}>
+                <label>
+                    <input type="checkbox" checked={done} />
+                </label>
+            </li>
+        )
+    }
+}
---
+++ .history/react-app/src/components/Item/index_20210713102151.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import './index.css'
+
+export default class Item extends Component {
+    //标识鼠标移入、移出的
+    state = {mouse:false} //标识鼠标移入、移出
+    handleMose = flag => {
+        return () => {
+            this.setState({mouse:flag})
+        }
+    }
+    render() {
+        const {id,name,done} = this.props
+        const {mouse} = this.state
+        return (
+            <li style={{background:mouse ? '#ddd' : 'white'}} onMouseEnter={this.handleMose(true)} onMouserLeave={this.handleMose(false)}>
+                <label>
+                    <input type="checkbox" checked={done}/>
+                    <span>{name}</span>
+                </label>
+                <button onClick={()=>this.handleDelete(id)} className='btn btn-danger' style={{display:mouse ? 'block' : 'none'}}>删除</button>
+            </li>
+        )
+    }
+}
